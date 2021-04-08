fe41fc935dfbd5c26ad67a06116a9a0fb0e4c64f
Taeke Roukema <roukema.taeke@gmail.com>
2021-04-08 10:28:36 +0200

    Integrated minimal vue.js

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+  el: '.banner-article',
+  data: {
+    titel: 'Ischa’s scheerroutine',
+    schrijver: 'Ischa Sachs',
+    datum: '07/04/2021',
+    samenvatting: 'Het scheren van een baard is niet zo makkelijk als je misschien denkt, in zijn dagelijkse blog laat Ischa zien hoe hij te werk gaat. In zijn eerste artikel bespreekt hij zijn routine...'
+  }
+})
