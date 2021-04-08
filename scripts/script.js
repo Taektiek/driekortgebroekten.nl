24712322b92fb32dec66e887727f104813bf5b8f
TaekeRoukema <62061406+TaekeRoukema@users.noreply.github.com>
2021-04-08 21:09:49 +0200

    Artikelen vue componenten gemaakt

--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,42 @@
-var bannerArticle = new Vue({
-  el: '.banner-article',
-  data: {
-    titel: 'Ischa’s scheerroutine',
-    schrijver: 'Ischa Sachs',
-    datum: '07/04/2021',
-    samenvatting: 'Het scheren van een baard is niet zo makkelijk als je misschien denkt, in zijn dagelijkse blog laat Ischa zien hoe hij te werk gaat. In zijn eerste artikel bespreekt hij zijn routine...'
-  }
+const app = Vue.createApp({})
+
+app.component('normal-article', {
+  props: [
+    'titel',
+    'schrijver',
+    'datum',
+    'samenvatting'
+  ],
+  template: `
+    <div class="normal-article">
+        <div class="content">
+            <h4>{{titel}}</h4>
+            <h5>Door <span class="info">{{schrijver}}</span> op <span class="info">{{datum}}</span></h5>
+            <p>{{samenvatting}}</p>
+        </div>
+    </div>
+  `
 })
 
-var normalArticle1 = new Vue({
-  el: '#article-1',
-  data: {
-    titel: 'Hoe ‘The Stand’ ons een les kan geven over klimaatverandering.',
-    schrijver: 'Taeke Roukema',
-    datum: '12/04/2021',
-    samenvatting: '‘The Stand’  van Stephen King gaat over een virus die de wereld overgaat en de chaos die daardoor volgt. Velen hebben het daarom al gekoppeld met de huidige coronacrisis. Maar wat zegt het boek over klimaatverandering en kunnen we daar ook iets van leren?'
-  }
+app.component('banner-article', {
+  props: [
+    'titel',
+    'schrijver',
+    'datum',
+    'samenvatting'
+  ],
+  template: `
+    <div class="banner-article">
+        <div class="content">
+            <h3 class="article-title">{{titel}}</h3>
+            <h5>Door <span class="info">{{schrijver}}</span> op <span class="info">{{datum}}</span></h5>
+            <p>{{samenvatting}}</p>
+            <button class="meerlezen">Meer lezen</button>
+        </div>
+    </div>
+  `
 })
 
-var normalArticle2 = new Vue({
-  el: '#article-2',
-  data: {
-    titel: 'Wat betekent basisinkomen voor de gemiddelde lesbische man?',
-    schrijver: 'Riemer Kerkstra',
-    datum: '05/04/2021',
-    samenvatting: 'Binnen linkse kringen is het basisinkomen al jaren een populair idee. Maar wat betekent het nou eigenlijk voor mij, de gemiddelde lesbische man?'
-  }
-})
+const vm = app.mount("body")
+
+console.log(vm)
