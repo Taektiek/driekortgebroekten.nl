990fbd5f3b79df8b0b45a57ca194edf8199b01df
Taeke Roukema <roukema.taeke@gmail.com>
2021-04-08 10:54:18 +0200

    Added articles

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,29 @@
-var app = new Vue({
+var bannerArticle = new Vue({
   el: '.banner-article',
   data: {
     titel: 'Ischa’s scheerroutine',
     schrijver: 'Ischa Sachs',
     datum: '07/04/2021',
     samenvatting: 'Het scheren van een baard is niet zo makkelijk als je misschien denkt, in zijn dagelijkse blog laat Ischa zien hoe hij te werk gaat. In zijn eerste artikel bespreekt hij zijn routine...'
+  }
+})
+
+var normalArticle1 = new Vue({
+  el: '#article-1',
+  data: {
+    titel: 'Hoe ‘The Stand’ ons een les kan geven over klimaatverandering.',
+    schrijver: 'Taeke Roukema',
+    datum: '12/04/2021',
+    samenvatting: '‘The Stand’  van Stephen King gaat over een virus die de wereld overgaat en de chaos die daardoor volgt. Velen hebben het daarom al gekoppeld met de huidige coronacrisis. Maar wat zegt het boek over klimaatverandering en kunnen we daar ook iets van leren?'
+  }
+})
+
+var normalArticle2 = new Vue({
+  el: '#article-2',
+  data: {
+    titel: 'Wat betekent basisinkomen voor de gemiddelde lesbische man?',
+    schrijver: 'Riemer Kerkstra',
+    datum: '05/04/2021',
+    samenvatting: 'Binnen linkse kringen is het basisinkomen al jaren een populair idee. Maar wat betekent het nou eigenlijk voor mij, de gemiddelde lesbische man?'
   }
 })
