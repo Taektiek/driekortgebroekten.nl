1c3cf2ae7485861a0ce84ca3eeb051a50fda212f
Riemer17 <riemer.kerkstra@gmail.com>
2021-04-13 20:53:53 +0200

    Welk deel van de navbar actief regelen vanuit view functie -In de view functie moet je aangeven welk deel van de navbar actief is. Dat doe je zo:  render_template("...html", naamvanonderdeel="active")

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,10 @@
 from flask import render_template
 from app import app
 
+# In de view functie moet je aangeven welk deel van de navbar actief is. Dat doe je zo:
+# render_template("...html", naamvanonderdeel="active")
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", blog="active")
