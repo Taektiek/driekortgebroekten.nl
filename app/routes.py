41c625ecfb03186389c245a0275cc89e1539dac2
Riemer17 <riemer.kerkstra@gmail.com>
2021-04-14 16:58:17 +0200

    Simpele login form - In forms.py wordt het formulier gemaakt. Dat wordt dan getoond op de /login - Er gebeurt dan nog niks, er wrodt alleen een bericht geflashd

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 # In de view functie moet je aangeven welk deel van de navbar actief is. Dat doe je zo:
 # render_template("...html", naamvanonderdeel="active")
@@ -18,3 +19,12 @@ def index():
                  "summary": "Binnen linkse kringen is het basisinkomen al jaren een populair idee. Maar wat betekent het nou eigenlijk voor mij, de gemiddelde lesbische man?"}
                 ]
     return render_template("index.html", blog="active", bannerarticle=bannerarticle, articles=articles)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login aangevraagd voor gebruiker {}, onthoud_mij={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template("login.html", form=form)
