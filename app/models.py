fcf9da2d021450d165022e0a6af80f539cb4a70f
Riemer17 <riemer.kerkstra@gmail.com>
2021-04-24 10:14:16 +0200

    Articles model toegevoegd
    - Article tabel aangemaakt
    - Many to one relatie tussen user en article

--- app/models.py
+++ app/models.py
@@ -1,12 +1,15 @@
 from app import db, login
+from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    articles = db.relationship('Article', backref='author')
 
     def __repr__(self):
         return 'User {}'.format(self.username)
@@ -17,6 +20,18 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
+
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    title = db.Column(db.String(256), index=True, unique=True)
+    subtitle = db.Column(db.String(256), index=True, unique=True)
+    summary = db.Column(db.String(1028), index=True, unique=True)
+    body = db.Column(db.Text())
+    url = db.Column(db.String(64), index=True, unique=True)
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
