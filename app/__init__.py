6affb108421320c096601f337e9602fa9b8fe89b
Riemer17 <riemer.kerkstra@gmail.com>
2021-04-25 19:58:58 +0200

    login_message en login_message_category vertaald in __init__.py
    - category = warning

--- app/__init__.py
+++ app/__init__.py
@@ -10,5 +10,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+login.login_message = "Om deze pagina te bekijken moet je ingelogd zijn"
+login.login_message_category = "warning"
 
 from app import routes, models
