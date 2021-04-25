4af55dcd3685db068f15ce17eb2093496e450749
Riemer17 <riemer.kerkstra@gmail.com>
2021-04-25 19:18:52 +0200

    Artikelen worden opgehaald vanuit database voor index
    - Eerste artikel wordt in de banner getoond
    - Met plaatjes gebeurt nog niks

--- app/routes.py
+++ app/routes.py
@@ -11,16 +11,8 @@ from werkzeug.urls import url_parse
 @app.route('/')
 @app.route('/index')
 def index():
-    bannerarticle = {"title": "Ischa’s scheerroutine", "author": "Ischa Sachs", "date": "07/03/2021","summary": "Het scheren van een baard is niet zo makkelijk als je misschien denkt, in zijn dagelijkse blog laat Ischa zien hoe hij te werk gaat. In zijn eerste artikel bespreekt hij zijn routine..."}
-    articles = [{"title": "Hoe ‘The Stand’ ons een les kan geven over klimaatverandering", "author": "Taeke Roukema", "date": "12/04/2021",
-                 "summary": "‘The Stand’ van Stephen King gaat over een virus die de wereld overgaat en de chaos die daardoor volgt. Velen hebben het daarom al gekoppeld met de huidige coronacrisis. Maar wat zegt het boek over klimaatverandering en kunnen we daar ook iets van leren?"},
-                {"title": "Wat betekent basisinkomen voor de gemiddelde lesbische man?", "author": "Riemer Kerkstra",
-                 "date": "05/04/2021",
-                 "summary": "Binnen linkse kringen is het basisinkomen al jaren een populair idee. Maar wat betekent het nou eigenlijk voor mij, de gemiddelde lesbische man?"},
-                {"title": "Wat betekent basisinkomen voor de gemiddelde lesbische man?", "author": "Riemer Kerkstra",
-                 "date": "05/04/2021",
-                 "summary": "Binnen linkse kringen is het basisinkomen al jaren een populair idee. Maar wat betekent het nou eigenlijk voor mij, de gemiddelde lesbische man?"}
-                ]
+    bannerarticle = Article.query.order_by(Article.timestamp.desc()).first()
+    articles = Article.query.order_by(Article.timestamp.desc()).all()[1:]
     return render_template("index.html", title='Blog', blogclass="active", bannerarticle=bannerarticle, articles=articles)
 
 @app.route('/login', methods=['GET', 'POST'])
